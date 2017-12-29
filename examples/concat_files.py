0a26755b9d8f419a74f47fc9c7a688f868b6e42f
Charles Li <charles.dt.li@gmail.com>
2017-12-28 20:25:00 -0500

    Add rest of README example code to examples

--- examples/concat_files.py
+++ examples/concat_files.py
@@ -7,6 +7,7 @@ def read_lines_in_file(filename):
         yield from fp
 
 
+# Using `Stream`
 def concat_files(dirname):
     return (
         Stream(os.listdir(dirname))
@@ -14,6 +15,22 @@ def concat_files(dirname):
             .map(lambda fname: f"{dirname}/{fname}")
             .bind(read_lines_in_file)
     )
+
+
+# Using generators
+def concat_files_gen(dirname):
+    fnames = os.listdir(dirname)
+    sorted_fnames = sorted(fnames)
+    fnames_with_dir = (f"{dirname}/{fname}" for fname in sorted_fnames)
+    for fname in fnames_with_dir:
+        yield from read_lines_in_file(fname)
+
+
+# a more concise way, but perhaps less readable
+# `sorted(os.listdir(dirname))` is very dense
+def concat_files_concise(dirname):
+    for fname in sorted(os.listdir(dirname)):
+        yield from read_lines_in_file(f"{dirname}/{fname}")
 
 
 def main():
