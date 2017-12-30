3dcce9d15eaa0c4bd9f9ea35af678ca71999dfbe
Charles Li <charles.dt.li@gmail.com>
2017-12-30 11:29:13 -0500

    Fix version and add description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 setup(
     name="genstream",
-    version="0.1",
+    version="0.0.1",
     packages=find_packages(),
     author="lidatong",
+    description="Construct stream pipelines backed by generators",
     license="Unlicense",
     keywords="chaining chain generators piping pipes transformations",
     python_requires=">=3.6"
