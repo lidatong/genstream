a7619affe913fc041fdefd1cba83780318ab11e8
Charles Li <charles.dt.li@gmail.com>
2017-12-30 11:38:02 -0500

    Bump patch version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="genstream",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     author="lidatong",
     description="Construct stream pipelines backed by generators",
