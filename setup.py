9047d53881244c205891f1ff5358a664a0c884cd
Charles Li <charles.dt.li@gmail.com>
2017-12-30 11:04:04 -0500

    Add setup.py metadata

--- setup.py
+++ setup.py
@@ -3,4 +3,8 @@ setup(
     name="genstream",
     version="0.1",
     packages=find_packages(),
+    author="lidatong",
+    license="Unlicense",
+    keywords="chaining chain generators piping pipes transformations",
+    python_requires=">=3.6"
 )
