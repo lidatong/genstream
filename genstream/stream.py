30d764f8c390c518f59e47a4ca5f5bc70bec9f76
Charles Li <charles.dt.li@gmail.com>
2017-12-30 11:34:55 -0500

    Fix issue with iterable vs iterator in stream functions

--- genstream/stream.py
+++ genstream/stream.py
@@ -18,61 +18,61 @@ class Stream(Generic[A]):
         return Stream(args)
 
     def head(self):
-        return next(self)
+        return next(self.xs)
 
     def tail(self):
-        next(self)
-        return self
+        next(self.xs)
+        return Stream(self.xs)
 
     def map(self, f: Callable[[A], B]) -> 'Stream[B]':
-        return Stream(map(f, self))
+        return Stream(map(f, self.xs))
 
     def filter(self, p: Callable[[A], bool]) -> 'Stream[A]':
-        return Stream(filter(p, self))
+        return Stream(filter(p, self.xs))
 
     def bind(self, f: Callable[[A], 'Stream[B]']) -> 'Stream[B]':
-        return Stream(chain.from_iterable(map(f, self)))
+        return Stream(chain.from_iterable(map(f, self.xs)))
 
     def reduce(self, f: Callable[[A, 'Stream[B]'], 'B']) -> 'B':
         return reduce(f, self.xs)
 
     def concat(self, ys):
-        return Stream(chain(self, ys))
+        return Stream(chain(self.xs, ys))
 
     def take(self, n: int) -> 'Stream[A]':
-        return Stream(islice(self, n))
+        return Stream(islice(self.xs, n))
 
     def drop(self, n: int) -> 'Stream[A]':
-        return Stream(islice(self, n, None))
+        return Stream(islice(self.xs, n, None))
 
     def takewhile(self, p: Callable[[A], bool]) -> 'Stream[A]':
-        return Stream(takewhile(p, self))
+        return Stream(takewhile(p, self.xs))
 
     def dropwhile(self, p: Callable[[A], bool]) -> 'Stream[A]':
-        return Stream(dropwhile(p, self))
+        return Stream(dropwhile(p, self.xs))
 
     # Key functions reserved for situations in which a preceding .map to the op
     # would not evaluate to the same result
 
     def sort(self, key=None):
-        return Stream(sorted(self, key=key))
+        return Stream(sorted(self.xs, key=key))
 
     # built-ins max/min don't handle key=None properly
 
     def max(self, key=None):
         key = (lambda x: x) if key is None else key
-        return Stream(max(self, key=key))
+        return Stream(max(self.xs, key=key))
 
     def min(self, key=None):
         key = (lambda x: x) if key is None else key
-        return Stream(min(self, key=key))
+        return Stream(min(self.xs, key=key))
 
     def distinct(self, key=None):
         key = (lambda x: x) if key is None else key
 
         def g():
             S = set()
-            for x in self:
+            for x in self.xs:
                 key_x = key(x)
                 if key_x not in S:
                     yield x
@@ -83,19 +83,19 @@ class Stream(Generic[A]):
     # Terminal operations
 
     def collect(self, to: Callable[[Iterable[A]], Iterable[A]]) -> Iterable[A]:
-        return to(self)
+        return to(self.xs)
 
     def sum(self):
-        return sum(self)
+        return sum(self.xs)
 
     def any(self):
-        return any(self)
+        return any(self.xs)
 
     def all(self):
-        return all(self)
+        return all(self.xs)
 
     def count(self):
-        return sum(1 for x in self)
+        return sum(1 for _ in self.xs)
 
 
 if __name__ == '__main__':
