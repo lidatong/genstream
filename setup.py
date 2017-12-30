32e468fe32f4fd70615ff75db851fe8b8135c551
Charles Li <charles.dt.li@gmail.com>
2017-12-30 18:13:30 -0500

    Update setup.py & add tests and examples to manifest

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
+
 setup(
     name="genstream",
     version="0.0.2",
-    packages=find_packages(),
+    packages=find_packages(exclude=("tests*",)),
     author="lidatong",
     description="Construct stream pipelines backed by generators",
     license="Unlicense",
     keywords="chaining chain generators piping pipes transformations",
-    python_requires=">=3.6"
+    python_requires=">=3.6",
+    extras_require={
+        "dev": ["pytest"]
+    },
+    include_package_data=True
 )
